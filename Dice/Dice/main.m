28d4d13fe40c68dd12039c6acd433b85f14a19c5
Zach Smoroden <zach@smoroden.com>
2016-05-04 10:05:59 -0700

    initial project creation

---
+++ Dice/Dice/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Dice
+//
+//  Created by Zach Smoroden on 2016-05-04.
+//  Copyright © 2016 Zach Smoroden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
